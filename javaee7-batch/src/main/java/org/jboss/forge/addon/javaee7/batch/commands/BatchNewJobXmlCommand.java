931214a37e05a57cae2c23d6a7a9702050c6bb7a
arun-gupta <arun.gupta@gmail.com>
2014-05-09 15:23:28 -0700

    identify classes for each CLI parameter that already honor the contract required by the Batch specification

--- javaee7-batch/src/main/java/org/jboss/forge/addon/javaee7/batch/commands/BatchNewJobXmlCommand.java
+++ javaee7-batch/src/main/java/org/jboss/forge/addon/javaee7/batch/commands/BatchNewJobXmlCommand.java
@@ -1,9 +1,18 @@
 package org.jboss.forge.addon.javaee7.batch.commands;
 
-import org.jboss.forge.addon.ui.command.AbstractUICommand;
+import org.jboss.forge.addon.parser.java.facets.JavaSourceFacet;
+import org.jboss.forge.addon.parser.java.resources.JavaResource;
+import org.jboss.forge.addon.parser.java.resources.JavaResourceVisitor;
+import org.jboss.forge.addon.projects.Project;
+import org.jboss.forge.addon.projects.ProjectFactory;
+import org.jboss.forge.addon.projects.ui.AbstractProjectCommand;
+import org.jboss.forge.addon.resource.visit.VisitContext;
 import org.jboss.forge.addon.ui.context.UIBuilder;
 import org.jboss.forge.addon.ui.context.UIContext;
 import org.jboss.forge.addon.ui.context.UIExecutionContext;
+import org.jboss.forge.addon.ui.hints.InputType;
+import org.jboss.forge.addon.ui.input.InputComponent;
+import org.jboss.forge.addon.ui.input.UICompleter;
 import org.jboss.forge.addon.ui.input.UIInput;
 import org.jboss.forge.addon.ui.input.UIInputMany;
 import org.jboss.forge.addon.ui.input.UISelectMany;
@@ -11,24 +20,73 @@ import org.jboss.forge.addon.ui.input.UISelectOne;
 import org.jboss.forge.addon.ui.metadata.UICommandMetadata;
 import org.jboss.forge.addon.ui.metadata.WithAttributes;
 import org.jboss.forge.addon.ui.util.Metadata;
-import org.jboss.forge.addon.ui.util.Categories;
 import org.jboss.forge.addon.ui.result.Result;
 import org.jboss.forge.addon.ui.result.Results;
+import org.jboss.forge.roaster.model.source.JavaClassSource;
+import org.jboss.forge.roaster.model.source.JavaSource;
 
+import java.io.FileNotFoundException;
 import java.lang.Override;
 import java.lang.Exception;
+import java.util.HashSet;
+import java.util.Set;
 
+import javax.batch.api.chunk.AbstractItemReader;
+import javax.batch.api.chunk.AbstractItemWriter;
+import javax.batch.api.chunk.ItemProcessor;
+import javax.batch.api.chunk.ItemReader;
+import javax.batch.api.chunk.ItemWriter;
 import javax.inject.Inject;
 
-public class BatchNewJobXmlCommand extends AbstractUICommand
+public class BatchNewJobXmlCommand extends AbstractProjectCommand
 {
 	
-	@Inject @WithAttributes(label = "ItemReader") UIInput<String> reader; 
-	@Inject UIInput<String> processor; 
-	@Inject UIInput<String> writer; 
+	private final class BatchUICompleter implements UICompleter<String> {
+		Class<?> baseClass;
+		Class<?> baseInterface;
+		
+		public BatchUICompleter(Class<?> baseClass, Class<?> baseInterface) {
+			this.baseClass = baseClass;
+			this.baseInterface = baseInterface;
+		}
+		
+		@Override
+		public Iterable<String> getCompletionProposals(UIContext context,
+				InputComponent<?, String> input, String value) {
+			final Set<String> result = new HashSet<>();
+			Project selectedProject = getSelectedProject(context);
+			selectedProject.getFacet(JavaSourceFacet.class).visitJavaSources(new JavaResourceVisitor() {
+
+				@Override
+				public void visit(VisitContext context,
+						JavaResource javaResource) {
+					try {
+						JavaSource<?> javaType = javaResource.getJavaType();
+						if (javaType.isClass() && 
+							(((JavaClassSource)javaType).hasInterface(baseInterface) ||
+									(baseClass != null && baseClass.getName().equals(((JavaClassSource)javaType).getSuperType()))
+									)) {
+							result.add(javaType.getQualifiedName());
+						}
+					} catch (FileNotFoundException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
+			});;
+			
+			return result;
+		}
+	}
+
+	@Inject @WithAttributes(label = "ItemReader", type=InputType.JAVA_CLASS_PICKER) UIInput<String> reader; 
+	@Inject @WithAttributes(label = "ItemProcessor", type=InputType.JAVA_CLASS_PICKER) UIInput<String> processor; 
+	@Inject @WithAttributes(label = "ItemWriter", type=InputType.JAVA_CLASS_PICKER) UIInput<String> writer; 
 	@Inject UIInputMany<String> inputMany;
 	@Inject UISelectOne<String> selectOne;
 	@Inject UISelectMany<String> selectMany;
+	
+	@Inject ProjectFactory projectFactory;
 
    @Override
    public UICommandMetadata getMetadata(UIContext context)
@@ -40,6 +98,9 @@ public class BatchNewJobXmlCommand extends AbstractUICommand
    @Override
    public void initializeUI(UIBuilder builder) throws Exception
    {
+	   reader.setCompleter(new BatchUICompleter(AbstractItemReader.class, ItemReader.class));
+	   processor.setCompleter(new BatchUICompleter(null, ItemProcessor.class));
+	   writer.setCompleter(new BatchUICompleter(AbstractItemWriter.class, ItemWriter.class));
 	   builder.add(reader).add(processor).add(writer);
    }
 
@@ -48,4 +109,14 @@ public class BatchNewJobXmlCommand extends AbstractUICommand
    {
       return Results.fail("Not implemented!");
    }
+
+	@Override
+	protected ProjectFactory getProjectFactory() {
+		return projectFactory;
+	}
+	
+	@Override
+	protected boolean isProjectRequired() {
+		return true;
+	}
 }
